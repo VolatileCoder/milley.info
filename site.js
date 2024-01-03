c8472457648510f36168e07ad8a46055a59cc6ba
VolatileCoder <mark.milley@binaryswitch.com>
2024-01-02 21:13:28 -0500

    attempt 4

--- site.js
+++ site.js
@@ -6,13 +6,6 @@
   function showPage(pageId){
     $(".page").hide();
     $(".preview").show();
-    document.body.scrollTop = document.documentElement.scrollTop = 0;
-    setTimeout(function(){showPageReally(pageId)},100);
-  }
-
-  function showPageReally(pageId){
-    
-    document.body.scrollTop = document.documentElement.scrollTop = 0;
     $(pageId).show();
     console.log(pageId + "Preview");
     $(pageId + "Preview").hide();
@@ -32,24 +25,13 @@
       default:
         document.title = "Welcome | Mark S. Milley";
     }
-    document.body.scrollTop = document.documentElement.scrollTop = 0;
   }
-  
 
   $(function(){
-    
-    $('.accordion-item').on('shown.bs.collapse', function () {
-      this.scrollIntoView();  
-    });
-
-    $(window).on('hashchange', function (e) {
-      showPage(location.hash);
-    });
-    
     if (window.location.hash) {
-        $(window).trigger('hashchange');
+      showPage(location.hash);
     }else {
-      window.location.hash = "intro";
+      showPage("#intro");
     }
 
   });
