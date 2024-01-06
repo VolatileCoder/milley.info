dd8843a6ca7884c5c55a367484a6bb8f2ca563cb
VolatileCoder <mark.milley@binaryswitch.com>
2024-01-06 07:26:57 -0500

    BB: paddle and control fixes.

--- blockbuster.js
+++ blockbuster.js
@@ -1,24 +1,49 @@
-var w = 900;
-var h = 1600;
+const game = {
+    width:900, 
+    height: 1600, 
+    paddle: {
+        width:160, 
+        height:40,
+        centerX:80,
+        top: 1600-160,
+    },
+};
 
-var main = Raphael("main",w,h);
-main.setViewBox(0, 0, w, h, true);
-main.canvas.setAttribute('preserveAspectRatio', 'meet');
-main.canvas.style.backgroundColor = '#334';
+game.screen = Raphael("main", game.width, game.height);
+game.screen.setViewBox(0, 0, game.width, game.height, true);
+game.screen.canvas.setAttribute('preserveAspectRatio', 'meet');
+game.screen.canvas.style.backgroundColor = '#334';
 
-var well = main.rect(0,0,900,1600);
+var well = game.screen.rect(0, 0, game.width, game.height);
 well.attr("fill","#000");
-well.mousemove(oninput);
-well.touchmove(oninput);
 
-var paddle = main.rect(0,0,160,40);
+var paddle = game.screen.rect(0, game.paddle.top, game.paddle.width, game.paddle.height);
 paddle.attr("fill","#fff");
+
+var killzone = game.screen.rect(0,game.paddle.top+game.paddle.height,game.width, game.height - (game.paddle.top+game.paddle.height));
+killzone.attr("fill","#800");
+
+var control = game.screen.rect(0, 0, game.width, game.height); //must declare last
+control.attr("fill","#F0F");
+control.attr("opacity",.0)
+
+
 
 function oninput(e,a){
     r = e.target.getBoundingClientRect();
-    console.log();
-    pos = ((e.clientX-r.x)/r.width)*well.attr("width")
-    if (!isNaN(pos)){
+    pos = ((e.clientX-r.x)/r.width) * game.width;
+    if (!isNaN(pos)) {
+        pos -= game.paddle.centerX
+        if(pos < 0) {
+            pos = 0;
+        } else if (pos + game.paddle.width > game.width) {
+            pos = game.width - game.paddle.width;
+            
+        }
+        
         paddle.attr("x",pos);
     }
 }
+
+control.mousemove(oninput);
+control.touchmove(oninput);
